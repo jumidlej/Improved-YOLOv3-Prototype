ca588a56cea22245e1eecb671ecbb4de68434340
Juliana Midlej <jmesanto91@gmail.com>
2021-01-15 13:41:15 -0300

    Convert YOLO augmented labels to training format

--- normalization/convert.py
+++ normalization/convert.py
@@ -5,16 +5,18 @@ from os import listdir
 yolo_labels_path = "/home/jacq/Documentos/Datasets/pcb_yolo/"
 pascal_labels_path = "/home/jacq/Documentos/Datasets/pcb_pascal/labels/"
 train_labels_path = "/home/jacq/Documentos/Datasets/pcb_train/labels/"
+augmented_labels_path = "/home/jacq/Documentos/Datasets/pcb_augmented/labels/"
 
 yolo_images_path = "/home/jacq/Documentos/Datasets/pcb_yolo/"
 pascal_images_path = "/home/jacq/Documentos/Datasets/pcb_pascal/images/"
 train_images_path = "/home/jacq/Documentos/Datasets/pcb_train/images/"
+augmented_images_path = "/home/jacq/Documentos/Datasets/pcb_augmented/images/"
 
 '''
 Argument: Images path (.jpg)
 Returns: List of images name
 '''
-def list_image_names(path=None):
+def list_images_name(path=None):
     if path == None:
         print("Nenhuma pasta foi especificada.")
         return 0
@@ -32,8 +34,8 @@ Argument: List of images name, dictionary with components and its numbers,
     list of excluded components
 Does: A .txt file to every image in yolo format
 '''
-def pascal_to_yolo_labelImg(image_names, components_dict, excluded_components):
-    for name in image_names:
+def pascal_to_yolo_labelImg(images_name, components_dict, excluded_components):
+    for name in images_name:
         image = cv2.imread(yolo_images_path+name+".jpg")
         yolo = open(yolo_labels_path+name+".txt", 'w')
 
@@ -79,9 +81,9 @@ Argument: List of images name, dictionary with components and its numbers,
 Does: A .txt file with every image and its bounding boxes in this format:
     image xmin,ymin,xmax,ymax,class_number xmin,ymin,xmax,ymax,class_number ...
 '''
-def pascal_to_yolo_training(image_names, components_dict, excluded_components):
+def pascal_to_training(images_name, components_dict, excluded_components):
     train_file = open("train.txt", 'w')
-    for name in image_names:
+    for name in images_name:
         train_file.write(train_images_path+name+".jpg")
 
         tree = ET.parse(pascal_labels_path+name+".xml")
@@ -116,8 +118,8 @@ Does: A .txt file to every image with this format:
     xmin, ymin, xmax, ymax, class_number
     in the folder "../augmentation/labels"
 '''
-def pascal_to_txt(image_names, components_dict, excluded_components):
-    for name in image_names:
+def pascal_to_txt(images_name, components_dict, excluded_components):
+    for name in images_name:
         train_file = open("../augmentation/labels/"+name+".txt", 'w')
 
         tree = ET.parse(pascal_labels_path+name+".xml")
@@ -143,6 +145,71 @@ def pascal_to_txt(image_names, components_dict, excluded_components):
                 train_file.write(str(xmin)+" "+str(ymin)+" "+str(xmax)+" "+str(ymax)+" "+str(components_dict[component_name]))
                 train_file.write("\n")
         train_file.close()
+
+'''
+Does: A .txt file with every image in the augmented_images_path in this format:
+    image xmin,ymin,xmax,ymax,class_number xmin,ymin,xmax,ymax,class_number ...
+'''
+def yolo_augmented_to_training():
+    images_name = []
+    files = [f for f in listdir(augmented_labels_path)]
+    for f in files:
+        if f[len(f)-4:] == ".txt":
+            images_name.append(f[:-4])
+
+    train_file = open("train_augmented.txt", 'w')
+    for name in images_name:
+        train_file.write(augmented_images_path+name+".jpg")
+        image = cv2.imread(augmented_images_path+name+".jpg")
+
+        labels_file = open(augmented_labels_path+name+".txt", "r")
+
+        for line in labels_file:
+            line = line.split()
+
+            x = float(line[1])
+            y = float(line[2])
+            w = float(line[3])
+            h = float(line[4])
+
+            px = image.shape[1]
+            py = image.shape[0]
+
+            xmin = int((x-w/2)*px)
+            ymin = int((y-h/2)*py)
+            xmax = int((x+w/2)*px)
+            ymax = int((y+h/2)*py)
+
+            train_file.write(" "+str(xmin)+","+str(ymin)+","+str(xmax)+","+str(ymax)+","+line[0])
+        train_file.write("\n")
+    train_file.close()
+
+'''
+Does: Test if the bounding boxes are correct in the training augmented file
+Problem: Basicly never stops so you have to kill it
+'''
+def test_yolo_augmented_to_training():
+    train = open("train_augmented.txt", "r")
+    for line in train:
+        line = line.split()
+        image = cv2.imread(line[0])
+        for i in range(1, len(line)):
+            line[i]=line[i].split(",")
+            xmin = int(line[i][0])
+            ymin = int(line[i][1])
+            xmax = int(line[i][2])
+            ymax = int(line[i][3])
+            image = cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (255,0,0), 2)
+        
+        scale_percent = 60 # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        
+        # resize image
+        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+        cv2.imshow("Imagem", resized)
+        cv2.waitKey(0)
 
 '''
 Argument: Name of xml file
@@ -180,10 +247,10 @@ def naive_normalization(name):
 Argument: List of images name
 Does: A classes.txt file with all components of all the images
 '''
-def normalization(image_names):
+def normalization(images_name):
     # create a empty set
     components = set()
-    for name in image_names:
+    for name in images_name:
         components = components.union(naive_normalization(name))
     
     # print(components)
@@ -227,14 +294,16 @@ def classes(classes_file):
 
     return components_dict, excluded
 
-image_names = list_image_names(pascal_images_path)
-print(image_names)
-print(len(image_names))
+# images_name = list_images_name(pascal_images_path)
+# print(images_name)
+# print(len(images_name))
 
-normalization(image_names)
-components_dict, excluded = classes("classes.txt")
-print(components_dict)
+# normalization(images_name)
+# components_dict, excluded = classes("classes.txt")
+# print(components_dict)
 
 # pascal_to_yolo_labelImg(images, components_dict, excluded)
-# pascal_to_yolo_training(image_names, components_dict, excluded)
-pascal_to_txt(image_names, components_dict, excluded)
+# pascal_to_yolo_training(images_name, components_dict, excluded)
+# pascal_to_txt(images_name, components_dict, excluded)
+# yolo_augmented_to_training()
+test_yolo_augmented_to_training()
