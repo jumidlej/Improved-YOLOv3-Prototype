15e21b264daac81501795aef6b88a48d6717ff0c
Juliana Midlej <jmesanto91@gmail.com>
2021-02-02 19:17:44 -0300

    Train and test split and size

--- tools/train_test_split.py
+++ tools/train_test_split.py
@@ -1,17 +1,29 @@
 import random
+import argparse
 
-test = 0.2
+ap = argparse.ArgumentParser()
+ap.add_argument("-sp", "--split", type=float,
+	help="Test size  0.0-1.0",  required=True)
+ap.add_argument("-sz", "--size", type=int,
+	help="Data size",  required=True)
+args = vars(ap.parse_args())
 
-ds = open("train_changed.txt", "r")
+txTest = args["split"]
+szTest = args["size"]
+
+ds = open("train_augmented.txt", "r")
 train_file = open("train_split.txt", "w")
 test_file = open("test_split.txt", "w")
 
 lines = ds.readlines()
 
+if(len(lines) < szTest):
+  szTest = len(lines)
+
 random.shuffle(lines)
 
-for i in range(len(lines)):
-    if i < len(lines)*0.2:
+for i in range(szTest):
+    if i < szTest*0.4:
         test_file.write(lines[i])
     else:
         train_file.write(lines[i])
@@ -19,4 +31,3 @@ for i in range(len(lines)):
 ds.close()
 train_file.close()
 test_file.close()
-
