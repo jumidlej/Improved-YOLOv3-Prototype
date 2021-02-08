9776c91a598f3c9af625edbd0bb6eb5e6185c95c
Juliana Midlej <jmesanto91@gmail.com>
2021-02-08 13:02:02 -0300

    Update

--- detection_custom.py
+++ detection_custom.py
@@ -17,7 +17,12 @@ from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_
 from yolov3.configs import *
 
 image_path   = "/home/jacq/Documentos/Datasets/pcb_pascal/images/Virtex6.jpg"
-output_path = "images/output.jpg"
+output_path = "images/test.jpg"
+#video_path   = "./IMAGES/test.mp4"
 
 yolo = Load_Yolo_model()
 detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
+
+#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
