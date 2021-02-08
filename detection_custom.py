ed3450c16c9181d0d25d538b659301171b21fe11
Juliana Midlej <jmesanto91@gmail.com>
2021-02-08 12:46:28 -0300

    Update

--- detection_custom.py
+++ detection_custom.py
@@ -17,11 +17,7 @@ from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_
 from yolov3.configs import *
 
 image_path   = "/home/jacq/Documentos/Datasets/pcb_pascal/images/Virtex6.jpg"
-# video_path   = "./IMAGES/test.mp4"
+output_path = "images/output.jpg"
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/plate_1_detect.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
+detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
--- detection_demo.py
+++ detection_demo.py
@@ -16,12 +16,8 @@ import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
 
-image_path   = "./IMAGES/kite.jpg"
-video_path   = "./IMAGES/test.mp4"
+image_path   = "/home/jacq/Documentos/Datasets/pcb_yolo/images/ACM-109_Bottom.jpg"
+output_path = "images/output.jpg"
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
+detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
